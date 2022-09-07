8b45edcfa80b626315946736b9534a38056d333d
JPDdeOliveira <jpos2@aluno.ifal.edu.br>
2022-09-07 00:20:44 +0000

    Refazendo conforme a revisão

--- Exercicios_em_dart/Atividade01.dart
+++ Exercicios_em_dart/Atividade01.dart
@@ -1,10 +1,13 @@
+//Função principal
 void main() {
     int valorMaior = maiorValor(1,2,3);
     print(valorMaior);
 }
 
+//Função para descobrir o maior valor dentre 3
 int maiorValor(int x, int y, int z){
     int maior = 0;
+    //Estrutura de decisão para prevenir erros
     if(x >= y && x >= z){
         maior = x;
     }else{
